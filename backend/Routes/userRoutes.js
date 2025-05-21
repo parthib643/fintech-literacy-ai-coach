d798223871f430719ff1e7a50678ae8ad11a239c
parthib643 <prathibh.aeng@zeetheta.com>
2025-05-21 11:52:01 +0530

    added userRoutes.js

---
+++ backend/Routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  registerUser,
+  loginUser,
+} = require("../controllers/userController");
+
+// @route   POST /api/users/register
+// @desc    Register a new user
+router.post("/register", registerUser);
+
+// @route   POST /api/users/login
+// @desc    Authenticate user & get token
+router.post("/login", loginUser);
+
+module.exports = router;
